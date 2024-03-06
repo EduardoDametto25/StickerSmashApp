7552eb05999221cc3f55e9b44e16264a1218b4b7
EduardoDametto <damettoed@gmail.com>
2024-03-06 12:22:45 -0300

    Finalização da estilização do componente Button e dowload do pacote do image-picker.

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import ImageViewer from './src/components/ImageViewer';
 import Button from './src/components/Button';
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -12,8 +13,8 @@ export default function App() {
         <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Escolher Foto"/>
-        <Button label="Escolher esta Foto"/>
+        <Button label="Escolher Foto" theme="primary"/>
+        <Button label="Usar esta Foto"/>
       </View>
       <StatusBar style="auto" />
     </View>
